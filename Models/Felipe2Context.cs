a7c53a2bd63a690063ec9fb8d0f1717c28eea133
Felipe-GB10 <fgbran2006@gmail.com>
2024-12-11 04:21:38 -0500

    Agregar archivos de proyecto.

---
+++ Models/Felipe2Context.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace GOLDENVFV.Models;
+
+public partial class Felipe2Context : DbContext
+{
+    public Felipe2Context()
+    {
+    }
+
+    public Felipe2Context(DbContextOptions<Felipe2Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Abono> Abonos { get; set; }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<DetalleReservaPaquete> DetalleReservaPaquetes { get; set; }
+
+    public virtual DbSet<DetalleReservaServicio> DetalleReservaServicios { get; set; }
+
+    public virtual DbSet<EstadosReserva> EstadosReservas { get; set; }
+
+    public virtual DbSet<Habitacione> Habitaciones { get; set; }
+
+    public virtual DbSet<ImagenAbono> ImagenAbonos { get; set; }
+
+    public virtual DbSet<ImagenHabitacion> ImagenHabitacions { get; set; }
+
+    public virtual DbSet<ImagenPaquete> ImagenPaquetes { get; set; }
+
+    public virtual DbSet<ImagenServicio> ImagenServicios { get; set; }
+
+    public virtual DbSet<Imagene> Imagenes { get; set; }
+
+    public virtual DbSet<MetodoPago> MetodoPagos { get; set; }
+
+    public virtual DbSet<Paquete> Paquetes { get; set; }
+
+    public virtual DbSet<PaqueteServicio> PaqueteServicios { get; set; }
+
+    public virtual DbSet<Permiso> Permisos { get; set; }
+
+    public virtual DbSet<PermisosRole> PermisosRoles { get; set; }
+
+    public virtual DbSet<Reserva> Reservas { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<Servicio> Servicios { get; set; }
+
+    public virtual DbSet<TipoDocumento> TipoDocumentos { get; set; }
+
+    public virtual DbSet<TipoHabitacione> TipoHabitaciones { get; set; }
+
+    public virtual DbSet<TipoServicio> TipoServicios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured)
+        {
+            // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+            //=> optionsBuilder.UseSqlServer("Server=DESKTOP-FQEK3J7\\SQLEXPRESS;Initial Catalog=Felipe2;integrated security=True; TrustServerCertificate=True");
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Abono>(entity =>
+        {
+            entity.HasKey(e => e.IdAbono).HasName("PK__Abono__A4693DA7CEADE5D9");
+
+            entity.ToTable("Abono");
+
+            entity.Property(e => e.CantAbono).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.FechaAbono)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.IdReservaNavigation).WithMany(p => p.Abonos)
+                .HasForeignKey(d => d.IdReserva)
+                .HasConstraintName("FK__Abono__IdReserva__17036CC0");
+        });
+
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.NroDocumento).HasName("PK__Clientes__CC62C91CF89EFEF2");
+
+            entity.Property(e => e.Apellidos)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Celular)
+                .HasMaxLength(12)
+                .IsUnicode(false);
+            entity.Property(e => e.Contrasena)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombres)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdRolNavigation).WithMany(p => p.Clientes)
+                .HasForeignKey(d => d.IdRol)
+                .HasConstraintName("FK__Clientes__IdRol__01142BA1");
+
+            entity.HasOne(d => d.IdTipoDocumentoNavigation).WithMany(p => p.Clientes)
+                .HasForeignKey(d => d.IdTipoDocumento)
+                .HasConstraintName("FK__Clientes__IdTipo__02084FDA");
+        });
+
+        modelBuilder.Entity<DetalleReservaPaquete>(entity =>
+        {
+            entity.HasKey(e => e.DetalleReservaPaquete1).HasName("PK__DetalleR__2E8BFF25FCDD6A58");
+
+            entity.ToTable("DetalleReservaPaquete");
+
+            entity.Property(e => e.DetalleReservaPaquete1).HasColumnName("DetalleReservaPaquete");
+            entity.Property(e => e.Costo).HasColumnType("decimal(18, 0)");
+
+            entity.HasOne(d => d.IdPaqueteNavigation).WithMany(p => p.DetalleReservaPaquetes)
+                .HasForeignKey(d => d.IdPaquete)
+                .HasConstraintName("FK__DetalleRe__IdPaq__123EB7A3");
+
+            entity.HasOne(d => d.IdReservaNavigation).WithMany(p => p.DetalleReservaPaquetes)
+                .HasForeignKey(d => d.IdReserva)
+                .HasConstraintName("FK__DetalleRe__IdRes__1332DBDC");
+        });
+
+        modelBuilder.Entity<DetalleReservaServicio>(entity =>
+        {
+            entity.HasKey(e => e.IdDetalleReservaServicio).HasName("PK__DetalleR__D3D91A5A30C19189");
+
+            entity.ToTable("DetalleReservaServicio");
+
+            entity.Property(e => e.Costo).HasColumnType("decimal(18, 0)");
+
+            entity.HasOne(d => d.IdReservaNavigation).WithMany(p => p.DetalleReservaServicios)
+                .HasForeignKey(d => d.IdReserva)
+                .HasConstraintName("FK__DetalleRe__IdRes__0F624AF8");
+
+            entity.HasOne(d => d.IdServicioNavigation).WithMany(p => p.DetalleReservaServicios)
+                .HasForeignKey(d => d.IdServicio)
+                .HasConstraintName("FK__DetalleRe__IdSer__0E6E26BF");
+        });
+
+        modelBuilder.Entity<EstadosReserva>(entity =>
+        {
+            entity.HasKey(e => e.IdEstadoReserva).HasName("PK__EstadosR__3E654CA8D4A55932");
+
+            entity.ToTable("EstadosReserva");
+
+            entity.Property(e => e.NombreEstadoReserva)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Habitacione>(entity =>
+        {
+            entity.HasKey(e => e.IdHabitacion).HasName("PK__Habitaci__8BBBF9012BA6033E");
+
+            entity.Property(e => e.Costo).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreHabitacion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdTipoHabitacionNavigation).WithMany(p => p.Habitaciones)
+                .HasForeignKey(d => d.IdTipoHabitacion)
+                .HasConstraintName("FK__Habitacio__IdTip__534D60F1");
+        });
+
+        modelBuilder.Entity<ImagenAbono>(entity =>
+        {
+            entity.HasKey(e => e.IdImagenAbono).HasName("PK__ImagenAb__A40FAE534AEF4A5D");
+
+            entity.ToTable("ImagenAbono");
+
+            entity.HasOne(d => d.IdAbonoNavigation).WithMany(p => p.ImagenAbonos)
+                .HasForeignKey(d => d.IdAbono)
+                .HasConstraintName("FK__ImagenAbo__IdAbo__1AD3FDA4");
+
+            entity.HasOne(d => d.IdImagenNavigation).WithMany(p => p.ImagenAbonos)
+                .HasForeignKey(d => d.IdImagen)
+                .HasConstraintName("FK__ImagenAbo__IdIma__19DFD96B");
+        });
+
+        modelBuilder.Entity<ImagenHabitacion>(entity =>
+        {
+            entity.HasKey(e => e.IdImagenHabi).HasName("PK__ImagenHa__5B5FF6ADBB0AF72A");
+
+            entity.ToTable("ImagenHabitacion");
+
+            entity.HasOne(d => d.IdHabitacionNavigation).WithMany(p => p.ImagenHabitacions)
+                .HasForeignKey(d => d.IdHabitacion)
+                .HasConstraintName("FK__ImagenHab__IdHab__71D1E811");
+
+            entity.HasOne(d => d.IdImagenNavigation).WithMany(p => p.ImagenHabitacions)
+                .HasForeignKey(d => d.IdImagen)
+                .HasConstraintName("FK__ImagenHab__IdIma__70DDC3D8");
+        });
+
+        modelBuilder.Entity<ImagenPaquete>(entity =>
+        {
+            entity.HasKey(e => e.IdImagenPaque).HasName("PK__ImagenPa__AD53DF94C3FC6546");
+
+            entity.ToTable("ImagenPaquete");
+
+            entity.HasOne(d => d.IdImagenNavigation).WithMany(p => p.ImagenPaquetes)
+                .HasForeignKey(d => d.IdImagen)
+                .HasConstraintName("FK__ImagenPaq__IdIma__7B5B524B");
+
+            entity.HasOne(d => d.IdPaqueteNavigation).WithMany(p => p.ImagenPaquetes)
+                .HasForeignKey(d => d.IdPaquete)
+                .HasConstraintName("FK__ImagenPaq__IdPaq__7C4F7684");
+        });
+
+        modelBuilder.Entity<ImagenServicio>(entity =>
+        {
+            entity.HasKey(e => e.IdImagenServi).HasName("PK__ImagenSe__3C03784C82134CEA");
+
+            entity.ToTable("ImagenServicio");
+
+            entity.HasOne(d => d.IdImagenNavigation).WithMany(p => p.ImagenServicios)
+                .HasForeignKey(d => d.IdImagen)
+                .HasConstraintName("FK__ImagenSer__IdIma__76969D2E");
+
+            entity.HasOne(d => d.IdServicioNavigation).WithMany(p => p.ImagenServicios)
+                .HasForeignKey(d => d.IdServicio)
+                .HasConstraintName("FK__ImagenSer__IdSer__778AC167");
+        });
+
+        modelBuilder.Entity<Imagene>(entity =>
+        {
+            entity.HasKey(e => e.IdImagen).HasName("PK__Imagenes__B42D8F2AF14EC0FF");
+        });
+
+        modelBuilder.Entity<MetodoPago>(entity =>
+        {
+            entity.HasKey(e => e.IdMetodoPago).HasName("PK__MetodoPa__6F49A9BEEDBB4C58");
+
+            entity.ToTable("MetodoPago");
+
+            entity.Property(e => e.NomMetodoPago)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Paquete>(entity =>
+        {
+            entity.HasKey(e => e.IdPaquete).HasName("PK__Paquetes__DE278F8BEBCD9BAF");
+
+            entity.Property(e => e.Costo).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.NomPaquete)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdHabitacionNavigation).WithMany(p => p.Paquetes)
+                .HasForeignKey(d => d.IdHabitacion)
+                .HasConstraintName("FK__Paquetes__IdHabi__5AEE82B9");
+        });
+
+        modelBuilder.Entity<PaqueteServicio>(entity =>
+        {
+            entity.HasKey(e => e.IdPaqueteServicio).HasName("PK__PaqueteS__DE5C2BC2F9A13018");
+
+            entity.ToTable("PaqueteServicio");
+
+            entity.Property(e => e.Costo).HasColumnType("decimal(18, 0)");
+
+            entity.HasOne(d => d.IdPaqueteNavigation).WithMany(p => p.PaqueteServicios)
+                .HasForeignKey(d => d.IdPaquete)
+                .HasConstraintName("FK__PaqueteSe__IdPaq__5DCAEF64");
+
+            entity.HasOne(d => d.IdServicioNavigation).WithMany(p => p.PaqueteServicios)
+                .HasForeignKey(d => d.IdServicio)
+                .HasConstraintName("FK__PaqueteSe__IdSer__5EBF139D");
+        });
+
+        modelBuilder.Entity<Permiso>(entity =>
+        {
+            entity.HasKey(e => e.IdPermiso).HasName("PK__Permisos__0D626EC85AF89B70");
+
+            entity.Property(e => e.NomPermiso)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<PermisosRole>(entity =>
+        {
+            entity.HasKey(e => e.IdPermisosRoles).HasName("PK__Permisos__8C257ED9C88563C6");
+
+            entity.HasOne(d => d.IdPermisoNavigation).WithMany(p => p.PermisosRoles)
+                .HasForeignKey(d => d.IdPermiso)
+                .HasConstraintName("FK__PermisosR__IdPer__4E88ABD4");
+
+            entity.HasOne(d => d.IdRolNavigation).WithMany(p => p.PermisosRoles)
+                .HasForeignKey(d => d.IdRol)
+                .HasConstraintName("FK__PermisosR__IdRol__4D94879B");
+        });
+
+        modelBuilder.Entity<Reserva>(entity =>
+        {
+            entity.HasKey(e => e.IdReserva).HasName("PK__Reservas__0E49C69DB7640F1C");
+
+            entity.Property(e => e.FechaFinalizacion).HasColumnType("datetime");
+            entity.Property(e => e.FechaInicio).HasColumnType("datetime");
+            entity.Property(e => e.FechaReserva)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.IdEstadoReserva).HasDefaultValueSql("('1')");
+            entity.Property(e => e.Iva)
+                .HasColumnType("decimal(18, 0)")
+                .HasColumnName("IVA");
+            entity.Property(e => e.MontoTotal).HasColumnType("decimal(18, 0)");
+
+            entity.HasOne(d => d.IdEstadoReservaNavigation).WithMany(p => p.Reservas)
+                .HasForeignKey(d => d.IdEstadoReserva)
+                .HasConstraintName("FK__Reservas__IdEsta__0A9D95DB");
+
+            entity.HasOne(d => d.MetodoPagoNavigation).WithMany(p => p.Reservas)
+                .HasForeignKey(d => d.MetodoPago)
+                .HasConstraintName("FK__Reservas__Metodo__0B91BA14");
+
+            entity.HasOne(d => d.NroDocumentoClienteNavigation).WithMany(p => p.Reservas)
+                .HasForeignKey(d => d.NroDocumentoCliente)
+                .HasConstraintName("FK__Reservas__IdEsta__09A971A2");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.IdRol).HasName("PK__Roles__2A49584CE7B8739A");
+
+            entity.Property(e => e.NomRol)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Servicio>(entity =>
+        {
+            entity.HasKey(e => e.IdServicio).HasName("PK__Servicio__2DCCF9A2FF111907");
+
+            entity.Property(e => e.Costo).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.NomServicio)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdTipoServicioNavigation).WithMany(p => p.Servicios)
+                .HasForeignKey(d => d.IdTipoServicio)
+                .HasConstraintName("FK__Servicios__IdTip__5812160E");
+        });
+
+        modelBuilder.Entity<TipoDocumento>(entity =>
+        {
+            entity.HasKey(e => e.IdTipoDocumento).HasName("PK__TipoDocu__3AB3332F053743AA");
+
+            entity.ToTable("TipoDocumento");
+
+            entity.Property(e => e.NomTipoDocumento)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<TipoHabitacione>(entity =>
+        {
+            entity.HasKey(e => e.IdTipoHabitacion).HasName("PK__TipoHabi__AB75E87C5EC644AD");
+
+            entity.Property(e => e.NombreTipoHabitacion)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<TipoServicio>(entity =>
+        {
+            entity.HasKey(e => e.IdTipoServicio).HasName("PK__TipoServ__E29B3EA78268CBD1");
+
+            entity.Property(e => e.NombreTipoServicio)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Habitacione.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GOLDENVFV.Models;
+
+public partial class Habitacione
+{
+    public int IdHabitacion { get; set; }
+
+    public int? IdTipoHabitacion { get; set; }
+
+    public string? NombreHabitacion { get; set; }
+
+    public bool? Estado { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public decimal? Costo { get; set; }
+
+    public virtual TipoHabitacione? IdTipoHabitacionNavigation { get; set; }
+
+    public virtual ICollection<ImagenHabitacion> ImagenHabitacions { get; set; } = new List<ImagenHabitacion>();
+
+    public virtual ICollection<Paquete> Paquetes { get; set; } = new List<Paquete>();
+}
